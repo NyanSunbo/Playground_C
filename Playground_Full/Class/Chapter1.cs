ec5175cce82749305f789dfeb8b05f58e9be4289
Nyan <Nyan@DESKTOP-KMNUMTP>
2022-03-26 10:41:38 +0700

    Add project files.

---
+++ Playground_Full/Class/Chapter1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Playground_Full
+{
+    internal class Chapter1
+    {
+        public class Student
+        {
+
+            public string Name { get; private set; }
+            public string LastName { get; private set; }
+            public List<int> CourseCodes { get; private set; }
+    
+            public Student(string studentNumber)
+            {
+                (Name, LastName) = GetStudentDetails(studentNumber);
+            }
+
+            public Student()
+            {
+
+            }
+
+            public void Deconstruct(out string name, out string lastName)
+            {
+                name = Name;
+                lastName = LastName;
+            }
+
+            private (string name, string surname) GetStudentDetails(string studentNumber)
+            {
+                var detail = (n: "Dirk", s: "Strauss");
+                return detail;
+            }
+
+        }
+
+
+
+
+    }
+}
---
+++ Playground_Full/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Playground_Full.Chapter1;
+
+namespace Playground_Full
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Chapter1 chapter1 = new Chapter1();
+
+            Student student = new Student("S20323742");
+            var (FirstName, Surname) = student;
+            Console.WriteLine($"The student name is {FirstName} {Surname}");
+        }
+    }
+}
